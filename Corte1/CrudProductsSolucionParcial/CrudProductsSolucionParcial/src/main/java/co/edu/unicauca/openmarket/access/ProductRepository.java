d4e67d07b8eda0f64ad4fa89b2f9bfd9db42c2e6
JSarabino <jsarabino@unicauca.edu.co>
2023-04-20 19:49:38 -0500

    Algunos cambios taller solid

---
+++ Corte1/CrudProductsSolucionParcial/CrudProductsSolucionParcial/src/main/java/co/edu/unicauca/openmarket/access/ProductRepository.java
@@ -0,0 +1,139 @@
+package co.edu.unicauca.openmarket.access;
+
+import co.edu.unicauca.openmarket.domain.Product;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Es una implementación que tiene libertad de hacer una implementación del
+ * contrato. Lo puede hacer con Sqlite, postgres, mysql, u otra tecnología
+ *
+ * @author Alex Torres
+ */
+public class ProductRepository implements IProductRepository {
+
+    private Connection conn;
+
+    public ProductRepository() {
+        initDatabase();
+    }
+
+    @Override
+    public boolean save(Product newProduct) {
+
+        try {
+            //Validate product
+            if (newProduct == null || newProduct.getName().isBlank() || newProduct.getDescription().isBlank()) {
+                return false;
+            }
+            //this.connect();
+
+            String sql = "INSERT INTO products ( name, description ) "
+                    + "VALUES ( ?, ? )";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, newProduct.getName());
+            pstmt.setString(2, newProduct.getDescription());
+            pstmt.executeUpdate();
+            //this.disconnect();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> products = new ArrayList<>();
+        try {
+
+            String sql = "SELECT * FROM products";
+            //this.connect();
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Product newProduct = new Product();
+                newProduct.setProductId(rs.getLong("productId"));
+                newProduct.setName(rs.getString("name"));
+                newProduct.setDescription(rs.getString("description"));
+
+                products.add(newProduct);
+            }
+            //this.disconnect();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+
+    private void initDatabase() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS products (\n"
+                + "	productId integer PRIMARY KEY AUTOINCREMENT,\n"
+                + "	name text NOT NULL,\n"
+                + "	description text NULL\n"
+                + ");";
+
+        try {
+            this.connect();
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+            //this.disconnect();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void connect() {
+        // SQLite connection string
+        //String url = "jdbc:sqlite:./myDatabase.db"; //Para Linux/Mac
+        //String url = "jdbc:sqlite:C:/sqlite/db/myDatabase.db"; //Para Windows
+        String url = "jdbc:sqlite::memory:";
+
+        try {
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public boolean edit(Long id, Product product) {
+        return true;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return true;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+}
